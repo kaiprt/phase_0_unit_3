f1dd02aabf02f31b9c3b1bb3cf6b0dda8fec9c47
Kai <kaiprt@gmail.com>
2014-05-04 11:07:26 -0400

    Grocery List

--- week_8_and_9/3_JavaScript/grocery_list/my_solution.js
+++ week_8_and_9/3_JavaScript/grocery_list/my_solution.js
@@ -1,7 +1,12 @@
 // U3.W8-9: 
 
 
-// I worked on this challenge [by myself, with: ].
+// I worked on this challenge by myself.
+// As a user I want to be able to add items AND quantity to a list.
+// As a user I want to be able to check off items on my list into a completed list.
+// As a user I want to be able to view my incomplete and complete list.
+// As a user I want to be able to remove items from my incomplete list.
+
 
 // 2. Pseudocode
 
@@ -20,6 +25,20 @@
 
 
 // 1. DRIVER TESTS/ASSERT STATEMENTS GO BELOW THIS LINE
+function assert(test, message, test_number) {
+	if (!test) {
+		console.log(test_number + "fasle");
+		throw "ERROR: " + message;
+	}
+	console.log(test_number + "true");
+	return true; 
+}
+
+assert(
+  (test),
+  "message",
+  "1. "
+  )
 
 
 
