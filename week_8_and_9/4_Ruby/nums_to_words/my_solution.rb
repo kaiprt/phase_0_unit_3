54da69f15d696052168c2056813b15d7d74f2014
Kai <kaiprt@gmail.com>
2014-05-05 14:35:24 -0400

    Submitting Rectangle Class and update nums to words

--- week_8_and_9/4_Ruby/nums_to_words/my_solution.rb
+++ week_8_and_9/4_Ruby/nums_to_words/my_solution.rb
@@ -4,12 +4,15 @@
 # I worked on this challenge [by myself, with: ].
 
 # 2. Pseudocode
-
-
+=begin
+1. Define a method, in_words, that takes an integer as an argument
+2. and have it return the spelled out word of the integer
+=end
 
 # 3. Initial Solution
 
-
+def in_words(num)
+  if num 
 
 
 # 4. Refactored Solution
@@ -20,10 +23,14 @@
 
 
 # 1. DRIVER TESTS/ASSERT STATEMENTS GO BELOW THIS LINE
+def assert
+  raise "Error Failed!" unless yield
+end
 
-
-
-
+assert { in_words(4) == "four" }
+assert { in_words(27) == "twenty seven" }  
+assert { in_words(92) == "ninety two" }
+assert { in_words(10) == "ten" }
 
 
 # 5. Reflection 
--- week_8_and_9/4_Ruby/rectangle_class/my_solution.rb
+++ week_8_and_9/4_Ruby/rectangle_class/my_solution.rb
@@ -1,13 +1,23 @@
 # U3.W8-9: Implement a Rectangle Class
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 2. Pseudocode
-
+=begin 
+1. Create an instance method, area, in the rectangle class
+2. and have it return the area of any Rectangle Class object.
+3. Create an instance method, perimeter, in the rectange class
+4. and have it return the perimeter in the rectangle class.
+5. Create an instance method, diagonal, in the rectange class
+6. and have it return the diagonal length of the object it's called on.
+7. Create an instance method, square?, in the rectange class
+8. and have it return if the rectangle can be classified as a square.
+=end
 
 
 # 3. Initial Solution
+=begin
 class Rectangle
   attr_accessor :width, :height
 
@@ -20,23 +30,82 @@ class Rectangle
     (other.width  == self.width && other.height == self.height ) ||
     (other.height == self.width && other.width  == self.height )
   end
+
+  def area
+    return @width * @height
+  end
+
+  def perimeter
+    ww = @width + @width
+    hh = @height + @height
+    return ww + hh
+  end
+
+  def diagonal
+    return Math.sqrt((@width**2)+(@height**2))
+  end
+
+  def square?
+    return @width == @height
+  end
 end
-
-
-
+=end
 
 # 4. Refactored Solution
+class Rectangle
+  attr_accessor :width, :height
 
+  def initialize(width, height)
+    @width  = width
+    @height = height
+  end
+
+  def ==(other)
+    (other.width  == self.width && other.height == self.height ) ||
+    (other.height == self.width && other.width  == self.height )
+  end
+
+  def area
+    return @width * @height
+  end
+
+  def perimeter
+    return @width + @width + @height + @height 
+  end
+
+  def diagonal
+    return Math.hypot(@width, @height)
+  end
+
+  def square?
+    return @width == @height
+  end
+
+end
 
 
 
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
+def assert
+  raise "Error Failed!" unless yield
+end
 
+rect = Rectangle.new(5, 5)
 
+assert { rect.area == 25 }
+assert { rect.perimeter == 20 }
+assert { rect.diagonal == 7.0710678118654755 }
+assert { rect.square? == true }
 
-
-
-
-# 5. Reflection 
+# 5. Reflection
+=begin
+ In this challenge I emplemented a basic Ruby class and created instance variables,
+ used built-in Ruby methods, and manipulated data. I felt very confident with the 
+ learning objectives and finished the challenge in a timely fashion. My goal when 
+ refactoring was to use the shortest amount of code possible to yield accurate 
+ results. This allowed me to explore methods in Ruby and I ended up gaining more 
+ experience with the class 'Math'. I enjoyed this challenge and the logic behind 
+ it.
+=end
