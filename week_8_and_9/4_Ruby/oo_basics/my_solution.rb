46fbbd78376e9de43b706e801f7bf92e1f8b9b87
Kai <kaiprt@gmail.com>
2014-05-06 15:44:34 -0400

    Submitting OO Basics and Whats the Scope (Ruby)

--- week_8_and_9/4_Ruby/oo_basics/my_solution.rb
+++ week_8_and_9/4_Ruby/oo_basics/my_solution.rb
@@ -1,10 +1,17 @@
 # U3.W8-9: OO Basics: Student
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 2. Pseudocode
-
+=begin
+1. Create an initialize method under the class Student that takes a first_name and scores as arguments.
+2. Create a method, average, that returns the average of a students scores.
+3. Create a method, letter_grade, that returns a students average letter grade.
+4. Create a seperate method, linear_search, that takes an array of students and a first name.
+5. Have it return -1 unless the first name is in the array of students then
+6. it should return the index of where the name resides in the array.
+=end
 
 
 # 3. Initial Solution
@@ -12,27 +19,60 @@
 class Student
   attr_accessor :scores, :first_name
 
-  def initialize(args)   #Use named arguments!
-    #your code here
+  def initialize(first_name, scores)
+    @first_name = first_name
+    @scores = scores 
+  end
+
+  def average
+    sum = 0
+    @scores.each { |i| sum += i }
+    return @average = sum / @scores.length
+  end
+
+  def letter_grade
+    if @average >= 90
+      "A"
+    elsif @average >= 80
+      "B"
+    elsif @average >= 70
+      "C"
+    elsif @average >= 60
+      "D"
+    else
+      "F"
+    end
   end
 end
 
+def linear_search(array, student)
+  names = []
+  array.each { |n| names << n.first_name}
+  unless names.include?(student)
+    return -1
+  else 
+    return names.index(student)
+  end
+end
 
+alex = Student.new("Alex", [100,100,100,0,100]) 
+dalton = Student.new("Dalton", [80,75,100,20,100])
+jenny = Student.new("Jenny", [100,100,100,100,100])
+kristina = Student.new("Kristina", [100,65,70,50,100])
+agnus = Student.new("Agnus", [100,100,100,80,100]) 
+students = [alex, dalton, jenny, kristina, agnus]
 # 4. Refactored Solution
+=begin 
+I would like to refactor this code and I am asking my classmates to help me find ways to cut some lines. :)
+=end
 
-
-
-
-
-
-# 1. DRIVER TESTS GO BELOW THIS LINE
+# 1. DRIV`ER TESTS GO BELOW THIS LINE
 # Tests for release 0:
 
 p students[0].first_name == "Alex"
 p students[0].scores.length == 5
 p students[0].scores[0] == students[0].scores[4]
 p students[0].scores[3] == 0
-
 
 # Tests for release 1:
 
@@ -44,8 +84,11 @@ p students[0].letter_grade == 'B'
 p linear_search(students, "Alex") == 0
 p linear_search(students, "NOT A STUDENT") == -1
 
-
-
-
-
 # 5. Reflection 
+=begin
+I found this challenge to be okay. I had trouble understanding the directions for Release 0 
+but I managed to work through this and find a solution. I used an unless statement for my 
+linear search compared to my standard if/else statement for variety and I had some fun 
+developing the code! :) Overall, I feel confident writing statements, creating objexts, and 
+automating repetitive tasks.
+=end
--- week_8_and_9/4_Ruby/what_the_scope/my_solution.rb
+++ week_8_and_9/4_Ruby/what_the_scope/my_solution.rb
@@ -12,10 +12,10 @@
 $var_1 = 22
  
 class Person
-  @@var_2
-  VAR_6 = "Ruby"
+  @@var_2 # Uninitialized class variable is this an object that needs to be called globally?
+  VAR_6 = "Ruby" # Should this be an instance variable? Why is this capitalized? Is it a constant?
  
-  attr_reader :var_3, :var_4
+  attr_reader :var_3, :var_4 # Why is this necessary?
  
   def initialize(var_5 = VAR_6)
     @var_3="Law of Demeter"  
@@ -29,16 +29,37 @@ class Person
 end
 
 # 3. Modified Code (with scope identified as comments)
+=begin 
+I am extremely confused with this challenge. The code here makes no sense.
+The majority of these objectects don't need to exist for this code to run.
+=end
 
+class Person
+  attr_reader :var_3, :var_4
+  #Removed @@var_2 because it's value is and always will be nil.
+  #changing VAR_6 to @var_6 so only this class can access it.
+  @var_6 = "Ruby" #Intance
 
+  def initialize(var_5 = @var_6) #var_5 is local @var_6 is instance.
+    @var_3 = "Law of Demeter" #Instance
+  end
 
+  def do_stuff(var_7=[1, 2, 3]) #Local
+    var_7.each do |var_8| #Local
+      var_9 += var_8 + 2 #Local
+    end
+  end
+end
 
 
 # 1. DRIVER TESTS/ASSERT STATEMENTS GO BELOW THIS LINE
-
-
-
-
-
+#Thix challenge did not call for driver tests.
 
 # 5. Reflection 
+=begin
+This was a very confusing challenge. I attempted to straighten things out while maintaining the
+integrity of the code but I am not sure if this is was what DBC wanted. I feel comfortable modifying
+code but I feel that this code REALLY made sense. I attempted my best to fix this and I will discuss
+with other students about there solutions. I commented out the scope as well as some initial questions
+I had with the code. 
+=end
