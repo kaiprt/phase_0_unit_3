c6d44958145287731b1c0566e1f57076dca651d3
Kai <kaiprt@gmail.com>
2014-05-07 17:29:44 -0400

    Update my_solution.js

--- week_8_and_9/3_JavaScript/grocery_list/my_solution.js
+++ week_8_and_9/3_JavaScript/grocery_list/my_solution.js
@@ -1,109 +1,67 @@
-// U3.W8-9: Challenge you're converting 
+// U3.W8-9: 
 
-// WEEK 6 -- PEZDISPENSER (SC)
 
-// class PezDispenser
+// I worked on this challenge by myself.
+// As a user I want to be able to add items AND quantity to a list.
+// As a user I want to be able to check off items on my list into a completed list.
+// As a user I want to be able to view my incomplete and complete list.
+// As a user I want to be able to remove items from my incomplete list.
 
-// 	def initialize(flavors)
-// 		@flavors = flavors 
-// 	end
-
-// 	def pez_count 
-// 		@flavors.length 
-// 	end 
-
-// 	def get_pez 
-// 		puts @flavors.first 
-// 		@flavors.slice!(0)
-// 	end 
-
-// 	def add_pez(new_flavor)
-// 		@flavors << new_flavor
-// 	end 
-
-// 	def see_all_pez 
-// 		puts @flavors 
-// 	end 
-
-// end
-
-// I worked on this challenge:
-// 1. Kai Prout
-// 2. Jake Huhn
 
 // 2. Pseudocode
+// Create class, Grocery, that takes a string and a quantity then
+// create class, grocerylist that takes an array and places grocery's into an array
+// 
 
-// Create a new function, PezDispenser, that initializes with the pezName variable
-// Set pezName and list equal to instance variables, this.list being an empty array
-// Create a new function addPez that takes a flavor and pushes it to the list. Return the result to the user
-// Create a new function getPez that pops the last pez from the array and returns this result
-// Create a new function pezCount that returns the result of taking the list.length
-// Create a new function seePez that iterates through the list via a for loop and returns the contents of the dispenser 
 
 // 3. Initial Solution
-
-function PezDispenser (pezName) {
-  this.pezName = pezName; 
-  this.list = [];
-  this.addPez = function (flavor) {
-    this.list.push(flavor);
-    console.log('You added ' + flavor + ' to the dispenser.');  
-  }; 
-  
-  this.getPez = function () { 
-    var flavorAte = this.list.pop();
-    this.list.pop();
-    console.log('You ate the ' + flavorAte + '.'); 
-  }; 
-  
-  this.pezCount = function () { 
-    var count = this.list.length;
-    console.log('The dispenser has ' + count + 'pez.'); 
-  }; 
-  
-  this.seePez = function () {
-    for (var i = 0; i < this.list.length; i++) {
-      console.log(this.list[i] + "\n");
-    }
-  };    
+function Grocery(name, quantity) {
+  this.name = name;
+  this.quantity = quantity;
 }
+
+function GroceryList() {
+  var incomplete = [];
+  this.addGrocery = function (object) {
+    incomplete.push(object);
+    console.log('Your ' + object.name + ' has been added.');
+  };
+  this.checkoff = function (object) {
+    var find = incomplete.indexOf(object);
+    incomplete.splice(find);
+    console.log('Checked off!');
+  };
+  this.viewList = function () {
+    for (var i = 0; i < incomplete.length; i++) {
+      console.log(incomplete[i].name + "<br />");
+    }
+  };
+}
+
 
 
 // 4. Refactored Solution
 
-function PezDispenser () {
-  this.list = [];
-  this.addPez = function (flavor) {
-    this.list.push(flavor);
-    console.log('You added ' + flavor + ' to the dispenser.');  
-  }; 
-  
-  this.getPez = function () { 
-    var flavorAte = this.list.pop();
-    this.list.pop();
-    console.log('You ate the ' + flavorAte + '.'); 
-  }; 
-  
-  this.pezCount = function () { 
-    var count = this.list.length;
-    this.count = count; 
-    console.log('The dispenser has ' + count + 'pez.'); 
-  }; 
-  
-  this.seePez = function () {
-    for (var i = 0; i < this.count; i++) {
-      console.log(this.list[i] + "\n");
-    }
-  };    
-}
+//I am not sure exactly what I could refactor here. Will update if I find a solution.
+
 // 1. DRIVER TESTS/ASSERT STATEMENTS GO BELOW THIS LINE
+orange = new Grocery("orange", 2);
+eggs = new Grocery("eggs", 24);
+butter = new Grocery("butter", 1);
+list = new GroceryList();
 
-dispenser1 = new PezDispenser();
+list.addGrocery(orange)
+list.addGrocery(eggs)
+list.addGrocery(butter)
+list.viewList
+list.checkoff(eggs)
+list.viewList
 
-dispenser1.addPez('lemon');
-dispenser1.pezCount();
-dispenser1.seePez();
-dispenser1.getPez(); 
-dispenser1.pezCount(); 
+
+
 
 // 5. Reflection 
+
+// This challenge helped me refresh on how to define functions, define local variables, create, add elements to, access, and iterate over arrays in JavaScript,
+// and create, add properties to, delete properties from, and access values from JavaScript Object literals. I felt OK with this challenge. I need to study more 
+// on JS syntax. I have an issue with my viewList function. I could not get the function to display my array conents.
